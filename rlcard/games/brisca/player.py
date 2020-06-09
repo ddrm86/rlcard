f599c5f433f7cb940045bf317dbd78c9dfa02862
David del Río Medina <ddrm86@gmail.com>
2020-06-09 14:19:47 +0200

    BriscaPlayer: - Fix objects sharing some members - Add cards legally known by opponents - Add equals

--- rlcard/games/brisca/player.py
+++ rlcard/games/brisca/player.py
@@ -3,8 +3,19 @@ from rlcard.core import Player
 
 class BriscaPlayer(Player):
     def __init__(self, player_id):
-        super().__init__(player_id)
+        self.player_id = player_id
+        self.hand = []
         self.score = 0
+        self.known_cards = []
+
+    def __eq__(self, other):
+        if isinstance(other, BriscaPlayer):
+            return self.player_id == other.player_id
+        else:
+            return NotImplemented
+
+    def __hash__(self):
+        return self.player_id * 43
 
     def available_order(self):
         raise NotImplementedError
