48aa3714369122a4de923e26a0f1f3d3f299f72c
David del Río Medina <ddrm86@gmail.com>
2020-06-16 10:31:46 +0200

    BriscaEnv: add basic observations (1-hot encoded)
    - Player's hand
    - Trump card
    - Board

--- rlcard/envs/brisca.py
+++ rlcard/envs/brisca.py
@@ -15,7 +15,7 @@ class BriscaEnv(Env):
         """
         self.game = Game()
         super().__init__(config)
-        self.state_shape = [2]
+        self.state_shape = [5, 40]
 
     def _get_legal_actions(self):
         raw_actions = self.game.get_actions(self.game.current_player)
@@ -29,9 +29,30 @@ class BriscaEnv(Env):
         return encoded_actions
 
     def _extract_state(self, state):
+        hand = list(state['hand'])
+        for i in range(3-len(hand)):
+            hand.append(None)
+        encoded_hand = [self._encode_card(BriscaCard.from_str_repr(card))
+                        if card is not None else self._encode_card(None) for card in hand]
+        board = state['board']
+        if len(board) == 0:
+            encoded_board = self._encode_card(None)
+        else:
+            encoded_board = self._encode_card(BriscaCard.from_str_repr(board[0]))
+        encoded_trump_card = self._encode_card(BriscaCard(state['trump_suit'], state['trump_rank']))
+        encoded_tuple = tuple(encoded_hand) + (encoded_board,) + (encoded_trump_card,)
+        obs = np.vstack(encoded_tuple)
         legal_actions = self._get_legal_actions()
-        extracted_state = {'obs': [], 'legal_actions': legal_actions}
+        extracted_state = {'obs': obs, 'legal_actions': legal_actions}
         return extracted_state
+
+    def _encode_card(self, card: BriscaCard or None):
+        num_cards = self.game.get_action_num() - 1  # discard substitute action
+        one_hot = np.zeros(num_cards, dtype=int)
+        if card is not None:
+            card_id = card.get_id()
+            one_hot[card_id] = 1
+        return one_hot
 
     def _decode_action(self, action_id):
         if action_id == self.enc_subs_action:
